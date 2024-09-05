3df20b720b6039a03e34fc8b1495b317686e06ac
take0x <89313929+take0x@users.noreply.github.com>
2024-09-06 02:02:06 +0900

    Add mypy and Ruff

--- src/md2tree/md2tree.py
+++ src/md2tree/md2tree.py
@@ -1,23 +1,23 @@
 import hashlib
 import subprocess
-from pathlib import Path
 import time
+from pathlib import Path
 
-header = """@startmindmap
+HEADER = """@startmindmap
 top to bottom direction
 
 """
 
-footer = """@endmindmap
+FOOTER = """@endmindmap
 """
 
 
 class FileWatcher:
-    def __init__(self, path):
+    def __init__(self, path: Path) -> None:
         self.path = path
         self.changed_count = 0
 
-    def watch(self):
+    def watch(self) -> None:
         content = self.path.read_text(encoding="utf-8")
 
         latest_hash = hashlib.md5(content.encode()).hexdigest()
@@ -36,7 +36,7 @@ class FileWatcher:
             time.sleep(0.1)
 
 
-def convert(mdpath):
+def convert(mdpath: Path) -> None:
     mdpath = Path(mdpath)
     pupath = mdpath.with_suffix(".pu")
 
@@ -61,35 +61,35 @@ def convert(mdpath):
 
 
 class Md2Pu:
-    def __init__(self, md):
+    def __init__(self, md: str) -> None:
         self.md = md
 
-    def convert(self):
-        return header + self.convert_md() + footer
+    def convert(self) -> str:
+        return HEADER + self.convert_md() + FOOTER
 
-    def convert_md(self):
+    def convert_md(self) -> str:
         pu = ""
         for line in self.md.split("\n"):
             pu += self.convert_md_line(line) + "\n"
         return pu
 
-    def convert_md_line(self, line):
+    def convert_md_line(self, line: str) -> str:
         line = line.replace("- ", "* ")
         line = line.replace("  ", "*")
         return line
 
 
 class Pu2Jpg:
-    def __init__(self, pupath) -> None:
+    def __init__(self, pupath: Path) -> None:
         self.pupath = pupath
 
-    def convert(self):
+    def convert(self) -> None:
         # java -jar .\plantuml.jar .\test.pu -charset UTF-8
-        command = [
+        command: list[str] = [
             "java",
             "-jar",
             "plantuml.jar",
-            self.pupath,
+            str(self.pupath),
             "-charset",
             "UTF-8",
         ]
@@ -114,10 +114,10 @@ if __name__ == "__main__":
         latest_hash = hash
 
         pu = Md2Pu(md).convert()
-        with open("sample.pu", "w") as f:
-            f.write(pu)
+        sample_pu = Path("sample.pu")
+        sample_pu.write_text(pu, encoding="utf-8")
 
         print("pu generated")
         print("convert to jpg")
-        Pu2Jpg("sample.pu").convert()
+        Pu2Jpg(sample_pu).convert()
         print("jpg generated")
